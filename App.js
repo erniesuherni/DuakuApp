b4aa977cfe66bbdccea72c0b80aa1cafbf3f4bb3
Suherni <6703@ait.nsw.edu.au>
2020-06-10 18:12:29 +1000

    create list of dua

--- App.js
+++ App.js
@@ -8,31 +8,33 @@ import {
   Alert,
   ScrollView,
   FlatList,
+  Dimensions
 } from 'react-native';
 
-export default class Home extends Component {
+var {height, width} = Dimensions.get('window');
+
+export default class Menu extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
+      //list of dua
       data: [
-        {id:1,  title: "You",      color:"#FF4500", image:"https://img.icons8.com/color/70/000000/name.png"},
-        {id:2,  title: "Home",     color:"#87CEEB", image:"https://img.icons8.com/office/70/000000/home-page.png"},
-        {id:3,  title: "Love",     color:"#4682B4", image:"https://img.icons8.com/color/70/000000/two-hearts.png"} ,
-        {id:4,  title: "Family",   color:"#6A5ACD", image:"https://img.icons8.com/color/70/000000/family.png"} ,
-        {id:5,  title: "Friends",  color:"#FF69B4", image:"https://img.icons8.com/color/70/000000/groups.png"} ,
-        {id:6,  title: "School",   color:"#00BFFF", image:"https://img.icons8.com/color/70/000000/classroom.png"} ,
-        {id:7,  title: "Things",   color:"#00FFFF", image:"https://img.icons8.com/dusk/70/000000/checklist.png"} ,
-        {id:8,  title: "World",    color:"#20B2AA", image:"https://img.icons8.com/dusk/70/000000/globe-earth.png"} ,
-        {id:9,  title: "Remember", color:"#191970", image:"https://img.icons8.com/color/70/000000/to-do.png"} ,
-        {id:10, title: "Game",     color:"#008080", image:"https://img.icons8.com/color/70/000000/basketball.png"} ,
+        {id:1,  title: "When waking up",      color:"#4682B4"},
+        {id:2,  title: "When wearing new clothes",     color:"#87CEEB"},
+        {id:3,  title: "When leaving home",     color:"#4682B4"} ,
+        {id:4,  title: "When beginning a meal",   color:"#87CEEB"} ,
+        {id:5,  title: "Upon entering home",  color:"#4682B4"} ,
+        {id:6,  title: "Before entering toilet",   color:"#87CEEB"} ,
+        {id:7,  title: "After leaving toilet",   color:"#4682B4"} ,
+        {id:8,  title: "Upon entering home",    color:"#87CEEB"} ,
+        {id:9,  title: "Before sleep", color:"#4682B4"} ,
+        {id:10, title: "Upon sneezing",     color:"#87CEEB"}
       ]
     };
   }
 
-  clickEventListener(item) {
-    Alert.alert(item.title)
-  }
+ 
 
   render() {
     return (
@@ -41,23 +43,15 @@ export default class Home extends Component {
           contentContainerStyle={styles.listContainer}
           data={this.state.data}
           horizontal={false}
-          numColumns={2}
           keyExtractor= {(item) => {
             return item.id;
           }}
           renderItem={({item}) => {
             return (
-              <View>
-                <TouchableOpacity style={[styles.card, {backgroundColor:item.color}]} onPress={() => {this.clickEventListener(item)}}>
-                  <Image style={styles.cardImage} source={{uri:item.image}}/>
-                </TouchableOpacity>
-
-                <View style={styles.cardHeader}>
-                  <View style={{alignItems:"center", justifyContent:"center"}}>
-                    <Text style={[styles.title, {color:item.color}]}>{item.title}</Text>
-                  </View>
-                </View>
-              </View>
+              <TouchableOpacity style={[styles.card, {backgroundColor:item.color}]} onPress={() => {this.clickEventListener(item)}}>
+                
+                <Text style={styles.title}>{item.title}</Text>
+              </TouchableOpacity>
             )
           }}/>
       </View>
@@ -68,68 +62,39 @@ export default class Home extends Component {
 const styles = StyleSheet.create({
   container:{
     flex:1,
-    marginTop:40,
-    backgroundColor:'#fff',
+    marginTop:20,
   },
   list: {
-    paddingHorizontal: 5,
-    backgroundColor:"#fff",
+    //paddingHorizontal: 5,
+    backgroundColor:"#E6E6E6",
   },
-  listContainer:{
-    alignItems:'center'
-  },
+
   /******** card **************/
   card:{
-    shadowColor: '#474747',
-    shadowOffset: {
-      width: 0,
-      height: 6,
-    },
-    shadowOpacity: 0.37,
-    shadowRadius: 7.49,
+    width: width,
+    height:150,
+    flexDirection:'row',
+    padding:20,
 
-    elevation: 12,
-    marginVertical: 20,
-    marginHorizontal: 40,
-    backgroundColor:"#e2e2e2",
-    //flexBasis: '42%',
-    width:120,
-    height:120,
-    borderRadius:60,
-    alignItems:'center',
-    justifyContent:'center'
+    justifyContent: 'center', 
+    alignItems: 'center' 
   },
-  cardHeader: {
-    paddingVertical: 17,
-    paddingHorizontal: 16,
-    borderTopLeftRadius: 1,
-    borderTopRightRadius: 1,
-    flexDirection: 'row',
-    alignItems:"center", 
-    justifyContent:"center"
-  },
-  cardContent: {
-    paddingVertical: 12.5,
-    paddingHorizontal: 16,
-  },
-  cardFooter:{
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    paddingTop: 12.5,
-    paddingBottom: 25,
-    paddingHorizontal: 16,
-    borderBottomLeftRadius: 1,
-    borderBottomRightRadius: 1,
-  },
-  cardImage:{
-    height: 50,
-    width: 50,
-    alignSelf:'center'
-  },
+  
   title:{
-    fontSize:24,
+    fontSize:28,
     flex:1,
-    alignSelf:'center',
-    fontWeight:'bold'
+    color:"#FFFFFF",
+    fontWeight:'bold',
+    marginLeft:40
   },
-});      
+  subTitle:{
+    fontSize:12,
+    flex:1,
+    color:"#FFFFFF",
+  },
+  icon:{
+    height: 20,
+    width: 20, 
+  }
+});     
+ 
