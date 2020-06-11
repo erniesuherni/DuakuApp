0951fd44007661c66b9a25c1e8893fa401aa48ac
Suherni <6703@ait.nsw.edu.au>
2020-06-11 21:35:06 +1000

    add component

--- components/Dua/index.js
+++ components/Dua/index.js
@@ -5,19 +5,19 @@ import { View, Text, Image } from 'react-native';
 import styles from './styles';
 
 
-//Define your class component
+//Define  class component
 class Dua extends PureComponent {
     static navigationOptions  = ({ navigation }) => ({
         title: `${navigation.getParam('title')} Info`
     })
     render() {
-        //Destruct your navigation props (NOT SETUP YET!)
+        //Destruct  navigation props (NOT SETUP YET!)
         const { navigation } = this.props;
         return (
             <View>
                
             {/*Use navigation.getParam to get the params of navigation prefered since you can set a default value
-            therefore not returning undefined and not crashing your app. */}
+            therefore not returning undefined and not crashing  app. */}
                 <Text style={styles.title}>{navigation.getParam('title')}</Text>
                 <Text style={styles.title}>{navigation.getParam('desc')}</Text>
                 <Text style={styles.title}>{navigation.getParam('pronuntiation')}</Text>
